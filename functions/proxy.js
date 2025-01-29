e4b31a21eaa027538145fda8841f6629836aa3a9
suho818 <20kikiki03@snu.ac.kr>
2025-01-30 05:44:26 +0900

    헤더수정정

--- functions/proxy.js
+++ functions/proxy.js
@@ -14,7 +14,11 @@ exports.handler = async function (event) {
     const response = await axios.get(url, {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept-Language": "ko-KR,ko;q=0.9",
+        "Referer": "https://namu.wiki/",
+        "DNT": "1",
+        "Connection": "keep-alive",
       },
     });
 
