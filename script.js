d39abaceae37f175f7597b0d95b29a73619770e0
suho818 <20kikiki03@snu.ac.kr>
2025-01-30 04:23:21 +0900

    수정정

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ async function loadWiki(url) {
      return;
   }
   try {
-    const response = await fetch(apiUrl, {
+    const response = await fetch("/api/wiki/이상혁", {
       headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
         Referer: "https://namu.wiki",
